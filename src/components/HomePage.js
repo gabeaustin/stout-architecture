0401236411cfe798c34812ad51c8c3b16b40fa37
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-06-14 19:32:44 -0500

    first commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <h1>Stout Architecture</h1>
+    </div>
+  );
+};
+
+export default HomePage;
