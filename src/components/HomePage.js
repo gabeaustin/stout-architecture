865f3a57eed7b04b370488d65ca5f00bceb8a753
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-06-16 18:06:05 -0500

    added media query to decrease font size of the company name on the home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,17 +3,23 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="container">
-      <div className="display-1 co-name-home-page text-uppercase text-center site-heading">
+      <div className="logo-container">
+      <div className="co-name-home-page text-uppercase text-center site-heading">
         Stout
       </div>
+      </div>
+
       <div className="display-4 co-byline-name-home-page text-lowercase text-center">
         architecture
       </div>
-      <div className="display-5 tag-line d-flex justify-content-center">
-        <p className="border w-50 text-center h5">
-          a birmingham-based architecture firm with experience in all aspects of
-          architectural design, management, and construction administration.
-        </p>
+      <div className="row">
+        <div className="tag-line d-flex justify-content-center">
+          <p className="border w-75 text-center">
+            a birmingham-based architecture firm with experience in all aspects
+            of architectural design, management, and construction
+            administration.
+          </p>
+        </div>
       </div>
     </div>
   );
