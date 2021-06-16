2e98cd41b03f767f968eedf4fb02d057e5a07c7e
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-06-16 18:39:16 -0500

    finished coding home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,15 +3,16 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="container">
-      <div className="logo-container">
-      <div className="co-name-home-page text-uppercase text-center site-heading">
-        Stout
-      </div>
+      <div className="logo-container center-screen">
+        <div className="co-name-home-page text-uppercase text-center site-heading">
+          Stout
+        </div>
+
+        <div className="display-4 co-byline-name-home-page text-lowercase text-center">
+          architecture
+        </div>
       </div>
 
-      <div className="display-4 co-byline-name-home-page text-lowercase text-center">
-        architecture
-      </div>
       <div className="row">
         <div className="tag-line d-flex justify-content-center">
           <p className="border w-75 text-center">
