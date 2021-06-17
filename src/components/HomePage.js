7c6646f693c4074f4eb4d01a174d03a1423c4e4a
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-06-16 22:28:12 -0500

    added needed margin to home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -13,14 +13,11 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div className="row">
-        <div className="tag-line d-flex justify-content-center">
-          <p className="border w-75 text-center">
-            a birmingham-based architecture firm with experience in all aspects
-            of architectural design, management, and construction
-            administration.
-          </p>
-        </div>
+      <div className="tag-line d-flex justify-content-center mb-5">
+        <p className="border-top border-bottom w-75 text-center mb-5">
+          a birmingham-based architecture firm with experience in all aspects of
+          architectural design, management, and construction administration.
+        </p>
       </div>
     </div>
   );
