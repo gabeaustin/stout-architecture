e51530e2be2216e719dd0626241d7ced01ed045c
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-06-17 16:39:23 -0500

    added people page and image folder that holds all the people images

--- src/components/About.js
+++ src/components/About.js
@@ -1,39 +1,44 @@
 import React from "react";
+import People from "./People";
 
 const About = () => {
   return (
-    <div className="container">
-      <div className="display-3 mb-4 site-heading">About Us</div>
-      <div className="row d-flex justify-content-md-around justify-content-lg-around justify-content-xl-around justify-content-sm-center">
-        <div className="card about-pg-card">
-          <div className="card-body">
-            <h5 className="card-title font-weight-bold site-heading hr-short">
-              mission
-            </h5>
-            <p className="card-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ut
-              soluta, recusandae, laborum ipsum ea at assumenda magnam incidunt,
-              dolores natus iure delectus enim sunt eius corporis fugit
-              molestiae dolorum?
-            </p>
+    <>
+      <div className="container">
+        <div className="display-3 mb-4 site-heading">About Us</div>
+        <div className="row d-flex justify-content-md-around justify-content-lg-around justify-content-xl-around justify-content-sm-center">
+          <div className="card about-pg-card">
+            <div className="card-body">
+              <h5 className="card-title font-weight-bold site-heading hr-short">
+                mission
+              </h5>
+              <p className="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ut
+                soluta, recusandae, laborum ipsum ea at assumenda magnam
+                incidunt, dolores natus iure delectus enim sunt eius corporis
+                fugit molestiae dolorum?
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div className="card about-pg-card">
-          <div className="card-body">
-            <h5 className="card-title font-weight-bold site-heading hr-short">
-              services
-            </h5>
-            <p className="card-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ut
-              soluta, recusandae, laborum ipsum ea at assumenda magnam incidunt,
-              dolores natus iure delectus enim sunt eius corporis fugit
-              molestiae dolorum?
-            </p>
+          <div className="card about-pg-card">
+            <div className="card-body">
+              <h5 className="card-title font-weight-bold site-heading hr-short">
+                services
+              </h5>
+              <p className="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ut
+                soluta, recusandae, laborum ipsum ea at assumenda magnam
+                incidunt, dolores natus iure delectus enim sunt eius corporis
+                fugit molestiae dolorum?
+              </p>
+            </div>
           </div>
-        </div>
-      </div> {/* this div ends the entire row */}
-    </div>
+        </div> {/* this div ends the entire row */}
+      </div>
+
+      <People />
+    </>
   );
 };
 
---
+++ src/components/People.js
@@ -0,0 +1,114 @@
+import React from "react";
+
+import Person1 from "../images/people/person-1.jpeg";
+import Person2 from "../images/people/person-2.jpeg";
+import Person3 from "../images/people/person-3.jpeg";
+import Person4 from "../images/people/person-4.jpeg";
+import Person5 from "../images/people/person-5.jpeg";
+import Person6 from "../images/people/person-6.jpeg";
+
+
+
+const People = () => {
+  return (
+    <div className="container">
+      <div className="display-3 mb-4 site-heading">People</div>
+
+      <div className="row border top-row-people d-flex justify-content-md-around justify-content-lg-around justify-content-xl-around justify-content-sm-center">
+        <div className="card people-pg-card">
+            <img src={Person1} class="card-img-top" alt="people"></img>
+          <div className="card-body">
+            <h5 className="card-title font-weight-bold site-heading hr-short">
+              mission
+            </h5>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ut
+              soluta, recusandae, laborum ipsum ea at assumenda magnam incidunt,
+              dolores natus iure delectus enim sunt eius corporis fugit
+              molestiae dolorum?
+            </p>
+          </div>
+        </div>
+
+        <div className="card people-pg-card">
+            <img src={Person2} class="card-img-top" alt="people"></img>
+          <div className="card-body">
+            <h5 className="card-title font-weight-bold site-heading hr-short">
+              mission
+            </h5>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ut
+              soluta, recusandae, laborum ipsum ea at assumenda magnam incidunt,
+              dolores natus iure delectus enim sunt eius corporis fugit
+              molestiae dolorum?
+            </p>
+          </div>
+        </div>
+
+        <div className="card people-pg-card">
+            <img src={Person3} class="card-img-top" alt="people"></img>
+          <div className="card-body">
+            <h5 className="card-title font-weight-bold site-heading hr-short">
+              mission
+            </h5>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ut
+              soluta, recusandae, laborum ipsum ea at assumenda magnam incidunt,
+              dolores natus iure delectus enim sunt eius corporis fugit
+              molestiae dolorum?
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="row border bottom-row-people d-flex justify-content-md-around justify-content-lg-around justify-content-xl-around justify-content-sm-center">
+        <div className="card people-pg-card">
+            <img src={Person4} class="card-img-top" alt="people"></img>
+          <div className="card-body">
+            <h5 className="card-title font-weight-bold site-heading hr-short">
+              mission
+            </h5>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ut
+              soluta, recusandae, laborum ipsum ea at assumenda magnam incidunt,
+              dolores natus iure delectus enim sunt eius corporis fugit
+              molestiae dolorum?
+            </p>
+          </div>
+        </div>
+
+        <div className="card people-pg-card">
+            <img src={Person5} class="card-img-top" alt="people"></img>
+          <div className="card-body">
+            <h5 className="card-title font-weight-bold site-heading hr-short">
+              mission
+            </h5>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ut
+              soluta, recusandae, laborum ipsum ea at assumenda magnam incidunt,
+              dolores natus iure delectus enim sunt eius corporis fugit
+              molestiae dolorum?
+            </p>
+          </div>
+        </div>
+
+        <div className="card people-pg-card">
+            <img src={Person6} class="card-img-top" alt="people"></img>
+          <div className="card-body">
+            <h5 className="card-title font-weight-bold site-heading hr-short">
+              mission
+            </h5>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ut
+              soluta, recusandae, laborum ipsum ea at assumenda magnam incidunt,
+              dolores natus iure delectus enim sunt eius corporis fugit
+              molestiae dolorum?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default People;
